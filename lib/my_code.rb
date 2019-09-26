a43b9db4c78222514b4fa58273bb0950af1226d3
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-26 21:18:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(array)
   return result_array
 end
 
-def reduce(array, starting_point)
+def reduce(array, starting_point=0)
   total = starting_point
   array.each do |item|
     total = yield(item,total)
